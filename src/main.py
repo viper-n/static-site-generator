a87ce19715785b8cef91d07378f2b6da278316f9
viper-n <github@mailsecure.mozmail.com>
2025-04-27 17:34:43 -0400

    2-2: TextNode Tests

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from textnode import TextNode, TextType
 
-def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
 
-if __name__ == "__main__":
-    main()
-    
+def main():
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
+
+
+main()
