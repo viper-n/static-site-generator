4e5bf8b5014222e307a561d4aa15285c0ea0ab40
viper-n <github@mailsecure.mozmail.com>
2025-04-30 15:26:02 -0400

    minor main.py change

--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
 
 
 def main():
     node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
-    props = {
-        "href": "https://www.google.com",
-        "target": "_blank",
-    }
-    html_child = HTMLNode("tag1", "value1")
-    html_node = HTMLNode("Tag", "Value", html_child, props)
-    print(html_node)
 
 
 main()
