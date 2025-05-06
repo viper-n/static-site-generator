659fc6c23b79eb06ca3ab00c7c429b674c13d8ba
viper-n <github@mailsecure.mozmail.com>
2025-05-06 17:45:07 -0400

    2-4: LeadNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,17 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        if value is None:
+            raise ValueError("LeafNode must have a value")
+        super().__init__(tag, value, [], props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+        if self.tag is None:
+            return self.value
+        else:
+            return f'<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,6 @@
 import unittest
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -48,6 +49,22 @@ class TestHTMLNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
         )
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+    
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_img(self):
+        node = LeafNode("img", "An image", {"src": "image.jpg", "alt": "description"})
+        self.assertEqual(node.to_html(), '<img src="image.jpg" alt="description">An image</img>')    
 
 
 if __name__ == "__main__":
