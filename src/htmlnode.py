9d79af74da24ab1e9a1b60f31b7e6ce3240546a1
viper-n <github@mailsecure.mozmail.com>
2025-05-07 11:53:24 -0400

    2-5: ParentNode (Recursive)

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,24 @@ class LeafNode(HTMLNode):
             return self.value
         else:
             return f'<{self.tag}{super().props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+        
