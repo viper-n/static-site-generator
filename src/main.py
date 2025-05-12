a0003732d198f7bce2e71a02fbdbaecff59ecb0a
viper-n <github@mailsecure.mozmail.com>
2025-05-12 15:03:17 -0400

    L3-4: extract links

--- src/main.py
+++ src/main.py
@@ -1,21 +1,9 @@
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+
 
 def main():
-    nodes = [
-        TextNode("This is *italic* and this is **bold**", TextType.TEXT)
-    ]
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    print(node)
 
-    # First, let's see the output when processing just the bold delimiter
-    bold_processed = split_nodes_delimiter(nodes, "**", TextType.BOLD)
-    print("After bold processing:")
-    for node in bold_processed:
-        print(f"- {node.text} ({node.text_type.name})")
-
-    # Then, we should process the italic delimiter on the result of the first pass
-    italic_processed = split_nodes_delimiter(bold_processed, "*", TextType.ITALIC)
-    print("\nAfter italic processing:")
-    for node in italic_processed:
-        print(f"- {node.text} ({node.text_type.name})")
 
 main()
